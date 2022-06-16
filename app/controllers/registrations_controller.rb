df935e219e8f2e7ccfc7d56efbc0dc6dc28b4081
Deepali Kumari <deepalikumari8054@gmail.com>
2022-06-16 16:47:35 +0530

    mailers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,15 +3,15 @@ class RegistrationsController < ApplicationController
         @user = User.new
     end
     def create
-        @user = User.new(user_params)
-
-        if @user.save
-        # stores saved user id in a session
-          session[:user_id] = @user.id
-          redirect_to root_path, notice: 'Successfully created account'
-        else
-          render :new
-        end
+      @user = User.new(user_params)
+      if @user.save
+        WelcomeMailer.with(user: @user).welcome_email.deliver_now
+        # deliver_now is provided by ActiveJob.
+        session[:user_id] = @user.id
+        redirect_to root_path, notice: 'Successfully created account'
+      else
+        render :new
+      end
     end
     private
     def user_params
